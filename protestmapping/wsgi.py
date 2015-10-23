b57c206fd7540cedf6a12822e3c7a1c24f1ddf32
Jay Mahabal <jmahabal@berkeley.edu>
2015-10-23 01:41:54 -0700

    pushing with old django imported django 2

--- protestmapping/wsgi.py
+++ protestmapping/wsgi.py
@@ -15,5 +15,6 @@ from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "protestmapping.settings")
 
+application = django.setup()
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
