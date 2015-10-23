ac27c7e41b20c84d84f4d44888353b9241f33452
Jay Mahabal <jmahabal@berkeley.edu>
2015-10-23 02:02:43 -0700

    trying to solve apps 3

--- protestmapping/settings.py
+++ protestmapping/settings.py
@@ -83,20 +83,10 @@ WSGI_APPLICATION = 'protestmapping.wsgi.application'
 
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        # username, password, etc...
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
