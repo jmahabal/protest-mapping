34aea0c5c157b123d3c767405e4d391077b9703e
Jay Mahabal <jmahabal@berkeley.edu>
2015-10-23 02:41:01 -0700

    trying to solve running 1

--- protestmapping/wsgi.py
+++ protestmapping/wsgi.py
@@ -10,12 +10,14 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 import django
 import django_postgrespool
+import dj_static
 
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "protestmapping.settings")
 
 application = django.setup()
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
 application = DjangoWhiteNoise(application)
